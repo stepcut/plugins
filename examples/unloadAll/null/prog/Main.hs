dcf8719ee5742a33188c4481a7fe16149a4cbc52
lemmih <lemmih@gmail.com>
2005-05-20 02:02:14 +0000

    Documentation fix for unloadAll and example for loading of a shared library (called loadCLib).

--- examples/unloadAll/null/prog/Main.hs
+++ examples/unloadAll/null/prog/Main.hs
@@ -9,7 +9,7 @@ main = do
     t     <- load_ "../Dep.o" ["../api"] "resource"
     case m_v of
         LoadFailure err -> error (unlines err)
-        LoadSuccess m v -> do putStrLn ( show (a v) ) ; unloadAll m
+        LoadSuccess m v -> do putStrLn ( show (a v) ) ; unloadAll m -- unloads Null.o but not Dep.o since we're still using it.
     case t of
         LoadFailure err -> error (unlines err)
         LoadSuccess m v -> do putStrLn ( show (a v) ) ; unloadAll m
